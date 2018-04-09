9a258c64eafa55353c7b47807cb921d9dbdfc504
michaelverwaayen <mverwaayen@outlook.com>
2018-04-09 17:39:39 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,10 @@
 <?php
 $link = mysqli_connect("localhost","root", "", "CPSC471");
-
 if(!$link)
 {
 	die('Cannot connect'. mysqli_error($link);
 	
 }
-
 if(isset($_POST['submit']))
 {
 	$uid = $_post['uid'];
@@ -14,13 +12,13 @@ if(isset($_POST['submit']))
 	
 	if(empty($uid) || empty($password))
 	{
-		
+		header("Location: ../learnphp.php?login=");
 	}
 	else
 	{
-		$query = “SELECT USER.ucredentials FROM HEALTHCARE_SYSTEM_USER AS USER WHERE USER. ucredentials = @USERNAME  AND USER.upassword = @PASSWORD”
-		$result = mysqli_query($link,$query);
-		$failure = mysqli_num_rows($result);
+		$query = "SELECT * FROM users WHERE ucredentials = '$uid'";
+		$result = mysql_query($link,$sql);
+		$failire = mysql_num_rows($result);
 		if($failure < 1)
 		{
 				header("Location: ../learnphp.php?login=sorry user or password wrong");
@@ -28,9 +26,9 @@ if(isset($_POST['submit']))
 		}
 		else
 		{
-			if($row = mysqli_fetch_assoc($result))
+			if($row = mysql_fetch_assoc($result))
 			{
-				$check =password_verify($password,$row['@PASSWORD']);
+				$check =password_verify($pasword,$row['upassword']);
 				if($check == false)
 				{
 					header("Location: ../learnphp.php?login=sorry user or password wrong");
@@ -39,7 +37,8 @@ if(isset($_POST['submit']))
 				elseif($check == true)
 				{
 					$SESSION['u_id'] = $row['ucredentials'];
-					$SESSION['u_first'] = $row['@Name'];
+					$SESSION['u_pass'] = $row['upassword'];
+
 				}
 				else{
 					header("Location: ../learnphp.php?login=sorry user or password wrong");
@@ -53,5 +52,6 @@ else
 	header("Location: ../learnphp.php?login=");
 	exit();
 }
-mysqli_close($link);
 }
+mysqli_close($link);
+?>
