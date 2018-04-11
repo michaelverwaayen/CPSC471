60d350afb95bd881206f6ee3e27139222e1ac3f6
michaelverwaayen <mverwaayen@outlook.com>
2018-04-10 21:13:22 -0600

    Update signuphelp.php

--- frontend/signuphelp.php
+++ frontend/signuphelp.php
@@ -1,50 +1,41 @@
 <? php_egg_logo_guid
-
+$link = mysqli_connect("localhost","root", "", "CPSC471");
+if(!$link)
+{
+	die('Cannot connect'. mysqli_error($link);
+	
+}
 if(isset($_POST['submit']))
 {
-	include_once 'db.php'
-	
-	$first = %_POST['first'];
-	$last = %_POST['last'];
 	$userName = %_POST['username'];
 	$password = %_POST['password'];
-	$confirmPassword = %_POST['confirmPassword'];
-	$role = %_POST['role'];
-	$email = %_POST['email'];
-	$uid = %POST['uid'];
 	//Handler
 	//MAke sure all entered
-	if((empty($first) || empty($last) || empty($userName) || 
-	empty($password)) || empty($confirmPassword) || empty($role) || empty($email))
+	if(empty($userName) || empty($password))
 	{
 		header("Location: ../signup.php<signup=empty");
 	}
 	else{
-		if(!preg_match("/^[a-zA-Z]*$"), $first || (!preg_match("/^[a-zA-Z]*$"), $last) || if(!preg_match("/^[a-zA-Z]*$"), $first, $last, $userName))
-		{
-			header("Location:../signup.php?signup=invalid_Character");
-			exit();
-		}	
-		if(!preg_match("/^[a-zA-Z1-9]*$"), $password || !preg_match("/^[a-zA-Z1-9]*$"), $confirmpassword )
-		{
-			header("Location:../signup.php?signup=invalid_Character");
-			exit();
-		}
-		if(!filter_var($email,FILTER_VALIDATE_EMAIL))
-		{
-			header("Location:../signup.php?signup=invalid_Email");
-			exit();
-		}
-		$sql = "SELECT * FROM users WHERE uid = '$uid' ";	
-		$result = mysql_query($conn,$sql);
+		
+		$query = SELECT USER.ucredentials FROM HEALTHCARE_SYSTEM_USER AS USER WHERE USER. ucredentials = @USERNAME  AND USER.upassword = @PASSWORD”;
+		$result = mysql_query($link,$query);
 		$failire = mysql_num_rows($result);
-		if($failire > 0)
+		if($failire < 1)
 		{
-			header("Location:../signup.php?signup=User name taken");
+			header("Location:../signup.php?signup=Something went wrong ");
 			exit();
 		}
-		$sql = "INSERT INTO users(user_first,user_last,user_email,user_uid,user_password) VALUES ('$first','$last','$email','$uid','$password'");
-		mysql_query($conn,$sql);
+		else
+		{
+			if($row = mysql_fetch_assoc($result))
+			{
+
+					$SESSION['u_id'] = $row['ucredentials'];
+					$SESSION['u_pass'] = $row['upassword'];
+				//Send to a new pg 
+			}
+
+		}
 	}
 	
 }
@@ -53,4 +44,5 @@ else
 		header("Location: ../signup.php");
 		exit();
 }
+mysqli_close($link);
 ?>
