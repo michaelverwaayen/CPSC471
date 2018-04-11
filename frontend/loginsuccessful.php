765bbd1ef293d0b06fb1c7f1789da8c63420dfed
michaelverwaayen <mverwaayen@outlook.com>
2018-04-10 21:21:41 -0600

    Update loginsuccessful.php

--- frontend/loginsuccessful.php
+++ frontend/loginsuccessful.php
@@ -1,12 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> HealthCare </title>
+		<h1> Log in successful </h1>
+	</head>
+</html>
 
-<?php
-$link = mysqli_connect("localhost","root", "", "CPSC471");
-if(!$link)
-{
-	die('Cannot connect'. mysqli_error($link);
-	
-}
-echo "Logged in";
-
-mysqli_close($link);
-?>
