ddfb13e19d18084ec8d28bf9a6354c48ecfdbf29
michaelverwaayen <mverwaayen@outlook.com>
2018-04-11 10:30:29 -0600

    Update loginsuccessful.php
    Once again trying to make the patient table appear when they log in... This is actually kind of tricky
    Gut feeling is doesnt work...

--- frontend/loginsuccessful.php
+++ frontend/loginsuccessful.php
@@ -5,4 +5,17 @@
 		<h1> Log in successful </h1>
 	</head>
 </html>
+<?php
+$link = mysqli_connect("localhost","root", "", "CPSC471");
+if(!$link)
+{
+	die('Cannot connect'. mysqli_error($link);
+	
+}
+echo "Information we have about you (the patient) <br>";
+include ('GetPatient.php');	    
+	    
+	    
 
+mysqli_close($link);
+?>
